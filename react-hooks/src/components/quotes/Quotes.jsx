2f8af22f3fb29493c57b0357650e82a97f30af7a
alvigabriel <claudio.alves20112011@hotmail.com>
2022-07-27 10:53:22 -0300

    React Hooks, explorando react styled um pouco mais, importando tipagem para propriedades

--- react-hooks/src/components/quotes/Quotes.jsx
+++ react-hooks/src/components/quotes/Quotes.jsx
@@ -1,9 +1,32 @@
+import styled from "styled-components";
+import { string } from 'prop-types'; //o tipo string foi importada pra poder tipar as propriedades 
+// da função (quotes), ou seja, espera-se sempre texto como propriedades, dando erro caso tenha outro tipo
+// sendo interessante para identificar melhor erros de nas propriedades
+
 export const Quotes = ({ quote, speaker}) => {
     return (
-    <div>        
-        <p>{quote}</p>
-        <p> - {speaker}</p>
+    <Wrapper>        
+        <Quote>{quote}</Quote>
+        <Speaker> - {speaker}</Speaker>
         <button>Quote No Jutsu</button>
-    </div>
+    </Wrapper>
     );
 }
+const Wrapper = styled.div`
+
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center
+`
+
+
+const Quote = styled.p`
+    font-size: 2em;    
+    margin: 0;
+`;
+
+const Speaker = styled(Quote)`
+    text-align: right;
+    margin-bottom: 50px;
+`;
